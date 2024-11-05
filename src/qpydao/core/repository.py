2eecb0289a5dae6bb72a848d3f683b6cc8bc4346
patrick <patrickwuke@163.com>
2024-11-05 15:05:18 +0800

    fix db qualifier name

--- src/qpydao/core/repository.py
+++ src/qpydao/core/repository.py
@@ -15,11 +15,11 @@ class RepositoryMeta(type):
         bases,
         attrs,
         base_type: SQLModel | BaseModel | Any = SQLModel,
-        db_qualify="default",
+        db_qualifier="default",
         **kwargs,
     ):
         x = super().__new__(cls, name, bases, attrs, **kwargs)
         x.base_type = base_type
-        x.db_qualifier = db_qualify
+        x.db_qualifier = db_qualifier
         x.db_client = databases.get_db(x.db_qualifier)
         return x
